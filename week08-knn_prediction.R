1b704ec4441e4b7f62dc623a913d944bd12739e4
lewin <9124350+lewinschmitt@users.noreply.github.com>
2023-04-27 11:57:27 +0200

    Update week08-knn_prediction.R

--- week08-knn_prediction.R
+++ week08-knn_prediction.R
@@ -1,15 +1,16 @@
+### Script prepared by Lewin Schmitt for IBEI's MLSS course (2023)
 ### This tutorial is adapted from Bruno Castanho Silva (2020), "Introduction to Machine Learning" (ECPR WS)
 ### The CHES data is from Polk, Jonathan, Jan Rovny, Ryan Bakker, Erica Edwards, Liesbet Hooghe, Seth Jolly, Jelle Koedam, Filip Kostelka, Gary Marks, Gijs Schumacher, Marco Steenbergen, Milada Anna Vachudova and Marko Zilovic. 2017. "Explaining the salience of anti-elitism and reducing political corruption for political parties in Europe with the 2014 Chapel Hill Expert Survey data," Research & Politics (January-March): 1-9.
 ### See https://www.chesdata.eu/ches-europe#2014-chapel-hill-expert-survey
 
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load(tidyverse, caret)
+pacman::p_load(tidyverse, caret, class)
 
 set.seed(123)
 
 # Let's load the data (make sure to have the csv file in your working directory if you want to work with local files!)
 data <- read.csv('2014_CHES_dataset_means.csv')
-data <- read.csv('https://www.chesdata.eu/s/2014_CHES_dataset_expert-level.csv')
+data <- read.csv('https://www.chesdata.eu/s/2014_CHES_dataset_means.csv')
 
 # For this exercise, we will only work with selected variables:
 data <- data %>%
@@ -30,13 +31,15 @@ cor(data$immigrate_policy, data$urban_rural, use = "complete.obs")
 plot(data$immigrate_policy, data$eu_position)
 cor(data$immigrate_policy, data$eu_position, use = "complete.obs")
 
+## Prediction task: EU stance ----
+
 # We want to see whether parties are pro- or anti-European (binary outcome)
-# need to transform the eu_position coding into a dichotomous variable 
-# anti = 0, pro = 1
+# Need to transform the eu_position coding into a dichotomous variable 
+# Coding rule: anti = 0, pro = 1
 median(data$eu_position)
 data <- data %>%
   mutate(eu_stance = ifelse(eu_position < 5.5, 'anti','pro'))
-table(data$eu_stance)
+table(data$eu_stance) # our outcome classes are nicely balanced (because we designed it this way)
 
 data %>%
   ggplot(aes(x = immigrate_policy, y = urban_rural, colour = eu_stance)) +
@@ -46,13 +49,13 @@ data %>%
 # Linear model won't work well here. How about a KNN?
 # KNN cannot take missing values, so need to filter these out.
 knn_data <- data %>%
-  select(eu_stance, urban_rural, immigrate_policy) %>%
+  select(eu_stance, urban_rural, immigrate_policy, environment) %>%
   na.omit()
 predictors <- knn_data[,c('urban_rural','immigrate_policy')]
-outcomes <- knn_data[,'eu_stance']
+outcomes <- factor(knn_data[,'eu_stance'])
 
-library(class)
-eu_predictions_knn <- knn(train = predictors, 
+is.na(predictors)
+eu_predictions_knn <- class::knn(train = predictors, 
                           test = predictors,
                    cl = outcomes,
                    k = 1)
@@ -64,25 +67,24 @@ table(eu_predictions_knn)
 table(eu_predictions_knn, outcomes)
 # And indeed, no misclassifications in the extremely flexible model of k=1
 # But this would likely be an extreme case of overfitting.
-# Let's increase k to decrease the flexibility:
 
-eu_predictions_knn_20 <- knn(train = predictors, 
+# So let's increase k to decrease the model flexibility:
+
+eu_predictions_knn_20 <- class::knn(train = predictors, 
                           test = predictors,
                           cl = outcomes, 
                           k = 20)
 table(eu_predictions_knn_20, outcomes)
 
 # We can use the caret package to get a confusion matrix and some prediction accuracy metrics:
-install.packages("caret")
-library(caret)
-confusionMatrix(eu_predictions_knn_20,factor(outcomes))
+caret::confusionMatrix(eu_predictions_knn_20,factor(outcomes))
 
 # Let's approach the search for k more systematically:
 results <- tibble(k = matrix(c(1:nrow(knn_data)),ncol=1),
                   accuracy = NA)
 
 for(k in 1:nrow(knn_data)){
-  eu_prediction.temp<-knn(train = predictors, test = predictors,
+  eu_prediction.temp<-class::knn(train = predictors, test = predictors,
                           cl = outcomes, k = k)
   results$accuracy[k] <-
     sum(eu_prediction.temp == outcomes)/length(eu_prediction.temp)
@@ -107,7 +109,7 @@ for(k in 1:(nrow(knn_data)-1)){
     test.predictors.loocv <- knn_data[lo,c('urban_rural','immigrate_policy')]
     train.out.loocv <- knn_data[-lo,'eu_stance']
     test.out.loocv <- knn_data[lo,'eu_stance']
-    predict_test_loocv <- knn(train = train.predictors.loocv,test = test.predictors.loocv,
+    predict_test_loocv <- class::knn(train = train.predictors.loocv,test = test.predictors.loocv,
                             cl = train.out.loocv, k = k)
     results2$match[lo] <- predict_test_loocv == test.out.loocv
   }
@@ -121,7 +123,14 @@ results1 %>%
 # What would be our best k based on LOOCV?
 max(results1$accuracy.loocv)
 
-## Exercises: ----
+# Note: the class() package with which we implemented this knn also has functions for CV:
+# check out the documentation for knn1() and knn.cv() to learn more about it.
+
+## Bonus exercises: ----
 # Does the prediction accuracy improve if you use other predictors (lrgen, galtan, environment)?
 # Can you use more than two simultaneously?
 # What about using 10-fold CV instead of LOOCV?
+
+## Voluntary take-home exercise ----
+# Use the best model you can train on the 2014 survey, and see how well it does on predicting EU stance with the more recent 2019 data.
+# Send me your (commented!) code ahead of next week's class. 
